f8a5328dc347ccfda1d3c57879db6b1d651ec90f
Eric Ranschau <eranschau@horizontalintegration.com>
2020-02-04 17:34:24 -0600

    NOJIRA: Testing Heroku build and deploy.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const proxy = require('express-http-proxy');
 
 const app = express();
 
 app.use(express.static('dist'));
+app.use('/inteerests', proxy('http://horizontal-ncpc-dev.herokuapp.com'));
+app.use('/profiles', proxy('http://horizontal-ncpc-dev.herokuapp.com'));
+app.use('/subscriptions', proxy('http://horizontal-ncpc-dev.herokuapp.com'));
 
 app.listen(process.env.PORT || 5000);
